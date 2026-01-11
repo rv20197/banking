55c1215be010a30ee254b05164574c6cd3708d94
Vatsal <rv20197@gmail.com>
2026-01-11 19:41:06 +0530

    File & Folder Structure

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   /* config options here */
 };
 
